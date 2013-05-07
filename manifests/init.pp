8bf4d1d6cadc8ec50553c6cc2d1852db5b0527f1
Glen Stampoultzis <gstamp@gmail.com>
2013-05-07 14:39:08 +1000

    Customized teamcity for our environment

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,3 @@
-# Class: teamcity
-#
-# This module manages teamcity
-#
-# Parameters:
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#
-# [Remember: No empty lines between comments and class definition]
 class teamcity {
 
 
